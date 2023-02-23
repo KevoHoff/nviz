5807cca62b89443ff98d6ddf243b29a8db6f937c
Kevin Hoffman <kehoffman@ursinus.edu>
2023-02-23 18:04:47 -0500

    gh pages update

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/el3um4s/memento-sveltekit-and-github-pages.git', // Update to point to your repository
+  repo: 'https://github.com/KevoHoff/nviz.git', // Update to point to your repository
   user: {
-   name: 'Samuele de Tomasi', // update to use your name
-   email: 'samuele@stranianelli.com' // Update to use your email
+   name: 'Kevin Hoffman', // update to use your name
+   email: 'kevohoffman@icloud.com' // Update to use your email
   },
   dotfiles: true
   },
