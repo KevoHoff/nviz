0243084699d1cbae7b02460ac2471cc571531b05
Kevin Hoffman <kehoffman@ursinus.edu>
2023-02-23 17:42:16 -0500

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
