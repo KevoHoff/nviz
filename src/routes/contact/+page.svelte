fae3abe16067db20d4392c1d016e26a6a1527afb
William Mongan <BillJr99@users.noreply.github.com>
2025-01-18 14:47:19 -0500

    Fixing image link

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,7 +7,7 @@
         <h1 class="h1">Contact us</h1>
         <p class="w-full text-center">Have any improvements you would like to suggest? Comments or concerns? Please send them our way and we will get back to you within 2 business days.</p>
         <a href="mailto:kevohoffman@icloud.com" class="border-2 rounded-full px-4 py-2 flex gap-2 hover:text-black transition-colors text-gray-600">
-            <img src="/mail.svg" alt="Mail">
+            <img src="../mail.svg" alt="Mail">
             <span>kevohoffman@icloud.com</span>
         </a>
 	</div>
